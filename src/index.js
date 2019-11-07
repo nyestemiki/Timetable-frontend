b6fbb7f5d3af9d2f2c0f1a2e340370e621746877
nyestemiki <nyeste.miki@yahoo.com>
2019-11-07 20:11:58 +0200

    Yoga Server

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+const server = createServer();
+
+server.start({
+    cors: {
+        credentials: true
+    }
+}, deets => {
+    console.log(`Server is running on port ${deets.port}`)
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
